a7e385fa27474b26fe1c0572147aa838eb1398f8
Alvaro Vega <avega@tid.es>
2015-05-21 12:24:01 +0200

    check if spassword plugin is enabled before use their SQL tables

--- keystone_spassword/contrib/spassword/backends/sql.py
+++ keystone_spassword/contrib/spassword/backends/sql.py
@@ -90,17 +90,18 @@ class Password(Driver):
 
 class Identity(Identity):
     def _check_password(self, password, user_ref):
-        # Check if password has been expired
-        session = sql.get_session()
-        spassword_ref = session.query(PasswordModel).get(user_ref['id'])
-        if not (spassword_ref == None):
-            # Check password time: 2 months
-            expiration_date = datetime.datetime.today() - \
-              datetime.timedelta(CONF.spassword.pwd_exp_days)
-            if (spassword_ref['creation_time'] < expiration_date):
-                LOG.error('password of user %s %s expired ' % (user_ref['id'],
-                                                               user_ref['name']))
-                # TODO: return False ?
+        if CONF.spassword.enabled:
+            # Check if password has been expired
+            session = sql.get_session()
+            spassword_ref = session.query(PasswordModel).get(user_ref['id'])
+            if not (spassword_ref == None):
+                # Check password time: 2 months
+                expiration_date = datetime.datetime.today() - \
+                  datetime.timedelta(CONF.spassword.pwd_exp_days)
+                if (spassword_ref['creation_time'] < expiration_date):
+                    LOG.error('password of user %s %s expired ' % (user_ref['id'],
+                                                                   user_ref['name']))
+                    # TODO: return False ?
 
         res = super(Identity, self)._check_password(password, user_ref)
         # TODO: Reset or increase login retries
@@ -111,20 +112,21 @@ class Identity(Identity):
     def authenticate(self, user_id, password):
         res = super(Identity, self).authenticate(user_id, password)
 
-        session = sql.get_session()
-        spassword_ref = session.query(PasswordModel).get(user_id)
+        if CONF.spassword.enabled:
+            session = sql.get_session()
+            spassword_ref = session.query(PasswordModel).get(user_id)
 
-        if spassword_ref:
-            if not res:
-                spassword_ref['login_attempts'] += 1
+            if spassword_ref:
+                if not res:
+                    spassword_ref['login_attempts'] += 1
 
-            res['extras'] = {
-                "password_creation_time": str(spassword_ref['creation_time']),
-                "login_attempts": spassword_ref['login_attempts']
-            }
-        #
-        # else:  # if no spassword_ref set creation time to now() ?
-        #
+                    res['extras'] = {
+                        "password_creation_time": str(spassword_ref['creation_time']),
+                        "login_attempts": spassword_ref['login_attempts']
+                        }
+            #
+            # else:  # if no spassword_ref set creation time to now() ?
+            #
         return res
 
         
