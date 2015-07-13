7d051272fc280aceebf1e87f6b3c97eeecc0c2fc
Alvaro Vega <avega@tid.es>
2015-07-13 11:43:01 +0200

    update user in spassword if aready exists (rare)

--- keystone_spassword/contrib/spassword/backends/sql.py
+++ keystone_spassword/contrib/spassword/backends/sql.py
@@ -67,12 +67,15 @@ class SPassword(Driver):
             data_user['creation_time'] = datetime.datetime.utcnow()
             data_user['domain_id'] = user['domain_id']
             spassword_ref = SPasswordModel.from_dict(data_user)
-            # A new session is needed
-            with session.begin():
-                session.add(spassword_ref)
         else:
+            # TODO: Never reached?
+            LOG.info('user %s already created in spassword, just updating' % user['id'])
             spassword_ref['creation_time'] = datetime.datetime.utcnow()
             spassword_ref['login_attempts'] = 0
+
+        # A new session is needed
+        with session.begin():
+            session.add(spassword_ref)
 
         return spassword_ref.to_dict()
 
