65eba54ebf98203481824d280917255fc660e006
Alvaro Vega <avega@tid.es>
2016-05-10 16:49:11 +0200

    fix blank spaces

--- docker/postlaunchconfig.sh
+++ docker/postlaunchconfig.sh
@@ -148,17 +148,17 @@ curl -X PUT http://${KEYSTONE_HOST}/v3/domains/${ID_ADMIN_DOMAIN}/users/${ID_CLO
       -H "Content-Type: application/json"
 
 curl -s -L --insecure https://github.com/openstack/keystone/raw/icehouse-eol/etc/policy.v3cloudsample.json \
-  | jq ' .["identity:scim_create_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"  
-     | .["identity:scim_list_roles"]="rule:cloud_admin or rule:admin_and_matching_domain_id"     
-     | .["identity:scim_get_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"       
-     | .["identity:scim_update_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"    
-     | .["identity:scim_delete_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"    
-     | .["identity:scim_get_service_provider_configs"]=""                                        
-     | .["identity:get_domain"]=""                                                               
-     | .admin_and_user_filter="role:admin and \"%\":%(user.id)%"                                 
-     | .admin_and_project_filter="role:admin and \"%\":%(scope.project.id)%"                     
-     | .["identity:list_role_assignments"]="rule:admin_on_domain_filter or rule:cloud_service or rule:admin_and_user_filter or rule:admin_and_project_filter"                                                                   
-     | .["identity:list_projects"]="rule:cloud_admin or rule:admin_and_matching_domain_id or rule:cloud_service" 
-     | .cloud_admin="rule:admin_required and domain_id:'${ID_ADMIN_DOMAIN}'"                      
-     | .cloud_service="rule:service_role and domain_id:'${ID_ADMIN_DOMAIN}'"'                     
+  | jq ' .["identity:scim_create_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
+     | .["identity:scim_list_roles"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
+     | .["identity:scim_get_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
+     | .["identity:scim_update_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
+     | .["identity:scim_delete_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
+     | .["identity:scim_get_service_provider_configs"]=""
+     | .["identity:get_domain"]=""
+     | .admin_and_user_filter="role:admin and \"%\":%(user.id)%"
+     | .admin_and_project_filter="role:admin and \"%\":%(scope.project.id)%"
+     | .["identity:list_role_assignments"]="rule:admin_on_domain_filter or rule:cloud_service or rule:admin_and_user_filter or rule:admin_and_project_filter"
+     | .["identity:list_projects"]="rule:cloud_admin or rule:admin_and_matching_domain_id or rule:cloud_service"
+     | .cloud_admin="rule:admin_required and domain_id:'${ID_ADMIN_DOMAIN}'"
+     | .cloud_service="rule:service_role and domain_id:'${ID_ADMIN_DOMAIN}'"' \
   | tee /etc/keystone/policy.json
