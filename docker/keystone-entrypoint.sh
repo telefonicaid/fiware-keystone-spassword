86a94e83e55e651ae92cedbbd096905d1c0a23a8
Alvaro Vega <avega@tid.es>
2016-08-10 11:45:04 +0200

    add logs to docker

--- docker/keystone-entrypoint.sh
+++ docker/keystone-entrypoint.sh
@@ -29,4 +29,5 @@ if [ "$DB_HOST_ARG" == "-dbhost" ]; then
 fi
 
 echo "[ keystone-entrypoint - keystone-all ] "
-/usr/bin/keystone-all
+/usr/bin/keystone-all &
+tail -f /var/log/keystone/keystone.log
