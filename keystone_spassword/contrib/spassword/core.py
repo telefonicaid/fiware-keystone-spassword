492c6ee7ab4ab4c364046ebf76d63b0cde74c6b6
Alvaro Vega <avega@tid.es>
2016-11-22 10:01:41 +0100

    fix spassword conf for keystone liberty version

--- keystone_spassword/contrib/spassword/core.py
+++ keystone_spassword/contrib/spassword/core.py
@@ -176,7 +176,7 @@ class SPassword(password.Password):
     def authenticate(self, context, auth_payload, user_context):
         """Try to authenticate against the identity backend."""
         if ('L' in versionutils.deprecated._RELEASES):
-            user_info = password.auth_plugins.UserAuthInfo.create(auth_payload, self.method)
+            user_info = password.auth_plugins.UserAuthInfo.create(auth_payload, 'password')
         else:
             user_info = password.UserAuthInfo.create(auth_payload)
 
