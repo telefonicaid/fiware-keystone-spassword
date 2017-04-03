371352b458fe9b8ac9d5e83b6f38f16b9227b173
Cesar Jorge <cjmartinez@aubay.es>
2017-04-03 15:57:52 +0200

    enable mysql port. Add timeout

--- docker/keystone-entrypoint.sh
+++ docker/keystone-entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-echo "[ keystone-entrypoint - starts... ] "
+
+echo "INFO: keystone entrypoint start"
 
 # Check argument DB_HOST
 DB_HOST_ARG=${1}
+# DB_HOST_VALUE can be hostname[:port]
 DB_HOST_VALUE=${2}
-DB_HOST_PORT=3306
+DB_HOST_NAME="$(echo "${DB_HOST_VALUE}" | awk -F: '{print $1}')"
+DB_HOST_PORT="$(echo "${DB_HOST_VALUE}" | awk -F: '{print $2}')"
+# Default MySQL port 3306
+[[ "${DB_HOST_PORT}" == "" ]] && DB_HOST_PORT=3306
+# DBTIMEOUT in seconds. Default to 60 seconds
+[[ "${DBTIMEOUT}" == "" ]] && export DBTIMEOUT=60
 
 DEFAULT_PASSWORD_ARG=${3}
 DEFAULT_PASSWORD_VALUE=${4}
@@ -12,23 +19,34 @@ DEFAULT_PASSWORD_VALUE=${4}
 MYSQL_PASSWORD_ARG=${5}
 MYSQL_PASSWORD_VALUE=${6}
 
-if [ "$DB_HOST_ARG" == "-dbhost" ]; then
-    # Wait until DB is up
-    #while ! nc -z $DB_HOST_VALUE $DB_HOST_PORT ; do sleep 10; done
-    while ! tcping -t 1 $DB_HOST_VALUE $DB_HOST_PORT; do sleep 10; done
+if [ "${DB_HOST_ARG}" == "-dbhost" ]; then
+    echo "INFO: MySQL endpoint <${DB_HOST_VALUE}>"
+    echo "INFO: DB_HOST_NAME <${DB_HOST_NAME}>"
+    echo "INFO: DB_HOST_PORT <${DB_HOST_PORT}>"
+    [[ "${DB_HOST_NAME}" == "" ]] && echo "ERROR: MySQL hostname not provided" >&2 && exit 2
+    # Wait until DB is up or exit if timeout
+    # Current time in seconds
+    STARTTIME=$(date +%s)
+    while ! tcping -t 1 ${DB_HOST_NAME} ${DB_HOST_PORT}
+    do
+        [[ $(($(date +%s) - ${DBTIMEOUT})) -lt ${STARTTIME} ]] || { echo "ERROR: Timeout MySQL endpoint <${DB_HOST_NAME}:${DB_HOST_PORT}>" >&2; exit 3; }
+        echo "INFO: Wait for MySQL endpoint <${DB_HOST_NAME}:${DB_HOST_PORT}>"
+        sleep 2
+    done
+
     # Check if postlaunchconfig was executed
     chkconfig openstack-keystone --level 3
     if [ "$?" == "1" ]; then
-        # Check if previos DB data exists
-        mysql -h $DB_HOST_VALUE -u root --password=$MYSQL_PASSWORD_VALUE -e 'use keystone'
+        # Check if previous DB data exists
+        mysql -s -h ${DB_HOST_NAME} -P ${DB_HOST_PORT} -uroot --password=${MYSQL_PASSWORD_VALUE} -e 'use keystone'
         if [ "$?" == "1" ]; then
-            /opt/keystone/postlaunchconfig.sh $DB_HOST_ARG $DB_HOST_VALUE $DEFAULT_PASSWORD_ARG $DEFAULT_PASSWORD_VALUE $MYSQL_PASSWORD_ARG $MYSQL_PASSWORD_VALUE
+            /opt/keystone/postlaunchconfig.sh ${DB_HOST_ARG} ${DB_HOST_VALUE} ${DEFAULT_PASSWORD_ARG} ${DEFAULT_PASSWORD_VALUE} ${MYSQL_PASSWORD_ARG} ${MYSQL_PASSWORD_VALUE}
         else
-            /opt/keystone/postlaunchconfig_update.sh $DB_HOST_ARG $DB_HOST_VALUE $DEFAULT_PASSWORD_ARG $DEFAULT_PASSWORD_VALUE $MYSQL_PASSWORD_ARG $MYSQL_PASSWORD_VALUE
+            /opt/keystone/postlaunchconfig_update.sh ${DB_HOST_ARG} ${DB_HOST_VALUE} ${DEFAULT_PASSWORD_ARG} ${DEFAULT_PASSWORD_VALUE} ${MYSQL_PASSWORD_ARG} ${MYSQL_PASSWORD_VALUE}
         fi
     fi
 fi
 
-echo "[ keystone-entrypoint - keystone-all ] "
+echo "INFO: keystone entrypoint - keystone-all"
 /usr/bin/keystone-all &
 tail -f /var/log/keystone/keystone.log
--- docker/postlaunchconfig_update.sh
+++ docker/postlaunchconfig_update.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-KEYSTONE_ADMIN_PASSWORD=4pass1w0rd
-MYSQL_ROOT_PASSWORD="iotonpremise"
+KEYSTONE_ADMIN_PASSWORD=keystone
+MYSQL_ROOT_PASSWORD=""
 
+# Check argument DB_HOST
 DB_HOST_ARG=${1}
+# DB_HOST_VALUE can be hostname[:port]
 DB_HOST_VALUE=${2}
+DB_HOST_NAME="$(echo "${DB_HOST_VALUE}" | awk -F: '{print $1}')"
+DB_HOST_PORT="$(echo "${DB_HOST_VALUE}" | awk -F: '{print $2}')"
+# Default MySQL port 3306
+[[ "${DB_HOST_PORT}" == "" ]] && DB_HOST_PORT=3306
 
 DEFAULT_PASSWORD_ARG=${3}
 DEFAULT_PASSWORD_VALUE=${4}
@@ -13,17 +19,16 @@ MYSQL_PASSWORD_ARG=${5}
 MYSQL_PASSWORD_VALUE=${6}
 
 if [ "$DEFAULT_PASSWORD_ARG" == "-default_pwd" ]; then
-    KEYSTONE_ADMIN_PASSWORD=$DEFAULT_PASSWORD_VALUE
+    KEYSTONE_ADMIN_PASSWORD=${DEFAULT_PASSWORD_VALUE}
 fi
 
 if [ "$MYSQL_PASSWORD_ARG" == "-mysql_pwd" ]; then
-    MYSQL_ROOT_PASSWORD="$MYSQL_PASSWORD_VALUE"
+    MYSQL_ROOT_PASSWORD="${MYSQL_PASSWORD_VALUE}"
 fi
 
 if [ "$DB_HOST_ARG" == "-dbhost" ]; then
     openstack-config --set /etc/keystone/keystone.conf \
-                     database connection mysql://keystone:keystone@$DB_HOST_VALUE/keystone;
-
+    database connection mysql://keystone:${KEYSTONE_ADMIN_PASSWORD}@$${DB_HOST_VALUE}/keystone;
 fi
 
 /usr/bin/keystone-all &
@@ -36,32 +41,32 @@ export OS_SERVICE_TOKEN=ADMIN
 export OS_SERVICE_ENDPOINT=http://127.0.0.1:35357/v2.0
 export KEYSTONE_HOST="127.0.0.1:5001"
 
-ID_ADMIN_DOMAIN=`mysql -h $DB_HOST_VALUE -u root --password=$MYSQL_PASSWORD_VALUE -e 'use keystone; select * from domain where name="admin_domain";' | awk '$2=="admin_domain" {print $1}'`
+ID_ADMIN_DOMAIN=`mysql -s -h ${DB_HOST_NAME} -P ${DB_HOST_PORT} -uroot --password=${MYSQL_PASSWORD_VALUE} -e 'use keystone; select * from domain where name="admin_domain";' | awk '$2=="admin_domain" {print $1}'`
 
 curl -s -L --insecure https://github.com/openstack/keystone/raw/liberty-eol/etc/policy.v3cloudsample.json \
   | jq ' .["identity:scim_create_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
-     | .["identity:scim_list_roles"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
-     | .["identity:scim_get_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
-     | .["identity:scim_update_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
-     | .["identity:scim_delete_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
-     | .["identity:scim_get_service_provider_configs"]=""
-     | .["identity:get_domain"]=""
-     | .admin_and_user_filter="role:admin and \"%\":%(user.id)%"
-     | .admin_and_project_filter="role:admin and \"%\":%(scope.project.id)%"
-     | .["identity:list_role_assignments"]="rule:cloud_admin or rule:admin_on_domain_filter or rule:cloud_service or rule:admin_and_user_filter or rule:admin_and_project_filter"
-     | .["identity:list_projects"]="rule:cloud_admin or rule:admin_and_matching_domain_id or rule:cloud_service"
-     | .cloud_admin="rule:admin_required and domain_id:'${ID_ADMIN_DOMAIN}'"
-     | .cloud_service="rule:service_role and domain_id:'${ID_ADMIN_DOMAIN}'"' \
-  | tee /etc/keystone/policy.json
+       | .["identity:scim_list_roles"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
+       | .["identity:scim_get_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
+       | .["identity:scim_update_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
+       | .["identity:scim_delete_role"]="rule:cloud_admin or rule:admin_and_matching_domain_id"
+       | .["identity:scim_get_service_provider_configs"]=""
+       | .["identity:get_domain"]=""
+       | .admin_and_user_filter="role:admin and \"%\":%(user.id)%"
+       | .admin_and_project_filter="role:admin and \"%\":%(scope.project.id)%"
+       | .["identity:list_role_assignments"]="rule:cloud_admin or rule:admin_on_domain_filter or rule:cloud_service or rule:admin_and_user_filter or rule:admin_and_project_filter"
+       | .["identity:list_projects"]="rule:cloud_admin or rule:admin_and_matching_domain_id or rule:cloud_service"
+       | .cloud_admin="rule:admin_required and domain_id:'${ID_ADMIN_DOMAIN}'"
+       | .cloud_service="rule:service_role and domain_id:'${ID_ADMIN_DOMAIN}'"' \
+       | tee /etc/keystone/policy.json
 
 /usr/bin/keystone-manage db_sync
 
 # Set another ADMIN TOKEN
 openstack-config --set /etc/keystone/keystone.conf \
-                 DEFAULT admin_token $KEYSTONE_ADMIN_PASSWORD
+    DEFAULT admin_token ${KEYSTONE_ADMIN_PASSWORD}
 
 
-kill -9 $keystone_all_pid
+kill -9 ${keystone_all_pid}
 sleep 3
 chkconfig openstack-keystone on
 
