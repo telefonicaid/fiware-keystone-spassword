f22762c8ddd8479196e3b6d75cd9e14fcc231579
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2018-03-14 11:11:09 +0100

    remove blank lines

--- keystone_spassword/contrib/spassword/migrate_repo/versions/002_add_sndfa_spassword_table.py
+++ keystone_spassword/contrib/spassword/migrate_repo/versions/002_add_sndfa_spassword_table.py
@@ -21,22 +21,20 @@ def upgrade(migrate_engine):
     # migrate_engine to your metadata
     meta = sql.MetaData()
     meta.bind = migrate_engine
-
     spassword_table = utils.get_table(migrate_engine, 'spassword')
-
     sndfa = sql.Column('sndfa', sql.Boolean(), default=False)
     sndfa_last = sql.Column('sndfa_last', sql.DateTime(), default=None)
     sndfa_code = sql.Column('sndfa_code', sql.String(32), default=None)
     sndfa_time_code = sql.Column('sndfa_time_code', sql.DateTime(), default=None)
     sndfa_email = sql.Column('sndfa_email', sql.Boolean(), default=False)
     sndfa_email_code = sql.Column('sndfa_email_code', sql.String(32), default=None)
-
     spassword_table.create_column(sndfa)
     spassword_table.create_column(sndfa_last)
     spassword_table.create_column(sndfa_code)
     spassword_table.create_column(sndfa_time_code)
     spassword_table.create_column(sndfa_email)
-    spassword_table.create_column(sndfa_email_code)    
+    spassword_table.create_column(sndfa_email_code)
+
 
 def downgrade(migrate_engine):
     meta = sql.MetaData()
@@ -48,6 +46,3 @@ def downgrade(migrate_engine):
     idp_table.drop_column('sndfa_time_code')
     idp_table.drop_column('sndfa_email')
     idp_table.drop_column('sndfa_email_code')
-
-
-
