ebc51f014c7c837fc676182919ea8b922aae0066
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2020-06-17 15:41:57 +0200

    Update keystone-entrypoint.sh

--- docker/keystone-entrypoint.sh
+++ docker/keystone-entrypoint.sh
@@ -44,5 +44,5 @@ crond &
 touch /var/log/keystone/keystone.log
 chmod 666 /var/log/keystone/keystone.log
 ln -snf /dev/stdout /var/log/keystone/keystone.log
-/usr/bin/keystone-all &
+/usr/bin/keystone-all
 
