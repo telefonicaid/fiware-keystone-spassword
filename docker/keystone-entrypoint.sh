9dbf48dbbf8a1d19b436e84a666ea5a332365685
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2024-02-09 12:20:52 +0100

    disable log std

--- docker/keystone-entrypoint.sh
+++ docker/keystone-entrypoint.sh
@@ -51,7 +51,7 @@ tail -17 /etc/keystone/keystone.conf
 touch /var/log/keystone/keystone.log
 chmod 777 /var/log/keystone/
 chmod 777 /var/log/keystone/keystone.log
-ln -snf /dev/stdout /var/log/keystone/keystone.log
+#ln -snf /dev/stdout /var/log/keystone/keystone.log
 echo "[ keystone-entrypoint - keystone-all ] "
 /usr/bin/keystone-all &
 
