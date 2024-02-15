d36bd24c77676fa5ea412e3ca67c6177a51a145a
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2024-02-15 11:29:38 +0100

    convert oslo-policy from json to yaml

--- docker/postlaunchconfig.sh
+++ docker/postlaunchconfig.sh
@@ -363,6 +363,12 @@ cat /opt/keystone/policy.v3cloudsample.json \
      | .cloud_service="rule:service_role and domain_id:'${ID_ADMIN_DOMAIN}'"' \
   | tee /etc/keystone/policy.json
 
+# Convert oslo-policy from json to yaml
+oslopolicy-convert-json-to-yaml --namespace keystone \
+  --policy-file /etc/keystone/policy.json \
+  --output-file /etc/keystone/policy.yaml
+
+
 # Set another ADMIN TOKEN
 openstack-config --set /etc/keystone/keystone.conf \
                  DEFAULT admin_token $KEYSTONE_ADMIN_PASSWORD
